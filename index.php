9479f1f43d9448de231e4c64682234e9cde5bc85
Pierre Boucher <Pierre Boucher@DESKTOP-MH1KFIO>
2018-10-01 14:56:10 +0200

    ajout du menu

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Bootstrap 101 Template</title>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HEP Campus chatbot</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- Bootstrap -->
-        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-      </head>
-      <body>
-        <h1>HEP</h1>
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top" style="font-size:1.5em;">
+            <div class="container" style="margin-top: 5px;margin-bottom: 5px;">
+                <img src="./images/LOGO_HEP_EDUCATION_BASELINE-FR.png" alt="" style="width: 20%;">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <script src="http://code.jquery.com/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
-      </body>
-    </html>
+    </body>
+</html>
