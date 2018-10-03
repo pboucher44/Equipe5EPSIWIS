0456bf4845bf837d6937ce8cde247372684b6469
TGuyot <guyot.thomas97@gmail.com>
2018-10-03 13:43:20 +0200

    fixed js

--- ChatBot/csvtoarray.js
+++ ChatBot/csvtoarray.js
@@ -1,8 +1,30 @@
 var tab = [];
 class csvtoarray {
 
+
     constructor() {
         var myvar;
+
+
+        function processData(allText) {
+            var allTextLines = allText.split(/\r\n|\n/);
+            var headers = allTextLines[0].split(',');
+            var lines = [];
+
+            for (var i = 1; i < allTextLines.length; i++) {
+                var data = allTextLines[i].split(',');
+                if (data.length == headers.length) {
+
+                    var tarr = [];
+                    for (var j = 0; j < headers.length; j++) {
+                        tarr.push(data[j]);
+                    }
+                    lines.push(tarr);
+                }
+            }
+            return lines;
+        }
+
         $.ajax({
             type: "GET",
             url: "data/bdd.csv",
@@ -13,26 +35,10 @@ class csvtoarray {
             }
         });
         tab = processData(myvar);
+
     }
 
-    processData(allText) {
-        var allTextLines = allText.split(/\r\n|\n/);
-        var headers = allTextLines[0].split(',');
-        var lines = [];
 
-        for (var i = 1; i < allTextLines.length; i++) {
-            var data = allTextLines[i].split(',');
-            if (data.length == headers.length) {
-
-                var tarr = [];
-                for (var j = 0; j < headers.length; j++) {
-                    tarr.push(data[j]);
-                }
-                lines.push(tarr);
-            }
-        }
-        return lines;
-    }
     getNom(index) {
 
         var nom = tab[index];
