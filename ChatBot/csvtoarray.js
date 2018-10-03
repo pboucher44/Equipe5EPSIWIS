ba620036fcf9e0e78cfdaa8bbf075645665b7c04
TGuyot <guyot.thomas97@gmail.com>
2018-10-03 13:39:59 +0200

    fixxing js

--- ChatBot/csvtoarray.js
+++ ChatBot/csvtoarray.js
@@ -2,37 +2,40 @@ var tab = [];
 class csvtoarray {
 
     constructor() {
-            $.ajax({
-                type: "GET",
-                url: "data/bdd.csv",
-                dataType: "text",
-                success: function (data) {
-                    tab = processData(data);
-                }
-            });
-        function processData(allText){
-            var allTextLines = allText.split(/\r\n|\n/);
-            var headers = allTextLines[0].split(',');
-            var lines = [];
-
-            for (var i = 1; i < allTextLines.length; i++) {
-                var data = allTextLines[i].split(',');
-                if (data.length == headers.length) {
-
-                    var tarr = [];
-                    for (var j = 0; j < headers.length; j++) {
-                        tarr.push(data[j]);
-                    }
-                    lines.push(tarr);
-                }
+        var myvar;
+        $.ajax({
+            type: "GET",
+            url: "data/bdd.csv",
+            dataType: "text",
+            async: false,
+            success: function (data) {
+                myvar = data;
             }
-            return lines;
+        });
+        tab = processData(myvar);
+    }
+
+    processData(allText) {
+        var allTextLines = allText.split(/\r\n|\n/);
+        var headers = allTextLines[0].split(',');
+        var lines = [];
+
+        for (var i = 1; i < allTextLines.length; i++) {
+            var data = allTextLines[i].split(',');
+            if (data.length == headers.length) {
+
+                var tarr = [];
+                for (var j = 0; j < headers.length; j++) {
+                    tarr.push(data[j]);
+                }
+                lines.push(tarr);
+            }
         }
+        return lines;
     }
     getNom(index) {
+
         var nom = tab[index];
         return nom
     }
 }
-
-
