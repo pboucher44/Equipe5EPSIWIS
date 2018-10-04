b061b8eacabb234417a5cf5b5fc119662e433e1a
TGuyot <guyot.thomas97@gmail.com>
2018-10-04 09:21:11 +0200

    functionnig buttons

--- ChatBot/ChatBot.js
+++ ChatBot/ChatBot.js
@@ -2,32 +2,29 @@
 
     class ChatBot {
 
-
-        constructor() {
-
+        constructor(pb) {
+            this.csv = pb;
         }
         printText(message, cssclass) {
             $("#chat").append("<div class=" + cssclass + "><p>" + message + "</p></div>");
         }
 
-        buttonAssociator(obj,csv) {
+        buttonAssociator(id) {
             $("#buttonsList").empty();
-            for (var i = 0; i < obj.length - 2; i++) {
-                $("#buttonsList").append("<div class=\"myButton\" onclick=\"changeContent("
-                    +obj[i + 2]+")\">" + (csv.getNom(obj[i+2]))[0] + "</div>");
+            for (var i = 0; i < pb.getNom(id).length - 2; i++) {
+                $("#buttonsList").append("<div class=\"myButton\" onclick=\"update(" + pb.getNom(id)[i + 2] + ")\">" +
+                    (this.csv.getNom(pb.getNom(id)[i + 2]-2))[0] + "</div>");
+                console.log(pb.getNom(id));
             }
 
         }
-        
-        readTextFile(file){
+
+        readTextFile(file) {
             var rawFile = new XMLHttpRequest();
             rawFile.open("GET", file, false);
-            rawFile.onreadystatechange = function ()
-            {
-                if(rawFile.readyState === 4)
-                {
-                    if(rawFile.status === 200 || rawFile.status == 0)
-                    {
+            rawFile.onreadystatechange = function () {
+                if (rawFile.readyState === 4) {
+                    if (rawFile.status === 200 || rawFile.status == 0) {
                         var allText = rawFile.responseText;
                         alert(allText);
                     }
