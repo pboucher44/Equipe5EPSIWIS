620cafc4a9595fba2d128c9efedfaf9cf1cfea59
mryod <mryod@LAPTOP-HLLO4EMB>
2018-10-04 11:05:10 +0200

    bouton Retour + recommencer visite

--- ChatBot/ChatBot.js
+++ ChatBot/ChatBot.js
@@ -1,46 +1,50 @@
-    var steps = ["step0", "step1", "step2", "step3", "step4", "step5", "step6", "step7"];
+var steps = ["step0", "step1", "step2", "step3", "step4", "step5", "step6", "step7"];
 
-    class ChatBot {
-
-        constructor(pb) {
-            this.csv = pb;
+class ChatBot {
+//ffgdfgdfdfgdfgdf
+    constructor(pb) {
+        this.csv = pb;
+    }
+    printText(message, cssclass) {
+        if (cssclass === "decal-gauche") {
+            $("#chat").append("<table><tr><td style=\"width:10%\"><img src=\"./images/bonhome.png\"  style=\"width:100%\"></img></td><td style=\"width:90%\"><div class=" + cssclass + "><p>" + message + "</p></div></td></tr></table>")
+        } else {
+            $("#chat").append("<div class=" + cssclass + "><p style=\"display: inline\">" + message + "</p></div>");
         }
-        printText(message, cssclass) {
-            if(cssclass==="decal-gauche"){
-                $("#chat").append("<table><tr><td style=\"width:10%\"><img src=\"./images/bonhome.png\"  style=\"width:100%\"></img></td><td style=\"width:90%\"><div class=" + cssclass + "><p>" + message + "</p></div></td></tr></table>")
-            }else{
-                $("#chat").append("<div class=" + cssclass + "><p style=\"display: inline\">" + message + "</p></div>");
-            }
+    }
+
+    buttonAssociator(id) {
+        $("#buttonsList").empty();
+        //           for (var i = 0; i < obj.length-2; i++) {
+        //              $("#buttonsList").append("<div class=\"myButton\" onclick=\"changeContent("
+        //                  +obj[i+2]+")\">" + (csv.getNom((obj[i+2])-2))[0] + "</div>");
+
+        for (var i = 0; i < pb.getNom(id).length - 2; i++) {
+            var suivant = (pb.getNom(id)[i + 2]) - 2;
+            $("#buttonsList").append("<div class=\"myButton\" onclick=\"update(" + suivant + ")\">" +
+                    (this.csv.getNom(pb.getNom(id)[i + 2] - 2))[0] + "</div>");
         }
-
-        buttonAssociator(id) {
-            $("#buttonsList").empty();
- //           for (var i = 0; i < obj.length-2; i++) {
-  //              $("#buttonsList").append("<div class=\"myButton\" onclick=\"changeContent("
-  //                  +obj[i+2]+")\">" + (csv.getNom((obj[i+2])-2))[0] + "</div>");
-
-            for (var i = 0; i < pb.getNom(id).length - 2; i++) {
-                var suivant = (pb.getNom(id)[i+2])-2;
-                $("#buttonsList").append("<div class=\"myButton\" onclick=\"update(" + suivant + ")\">" +
-                    (this.csv.getNom(pb.getNom(id)[i + 2]-2))[0] + "</div>");
-            }
-
-        }
-
-        readTextFile(file) {
-            var rawFile = new XMLHttpRequest();
-            rawFile.open("GET", file, false);
-            rawFile.onreadystatechange = function () {
-                if (rawFile.readyState === 4) {
-                    if (rawFile.status === 200 || rawFile.status == 0) {
-                        var allText = rawFile.responseText;
-                        var div = document.getElementById("chat");
-                        cb = new ChatBot();
-                        cb.printText(allText,"decal-gauche");
-                    }
-                }
-            }
-            rawFile.send(null);
+        if (id > 0) {
+            $("#buttonsList").append("<div class=\"myButton\" onclick=\"update(" + (id - 1) + ")\">Retour</div>");
+            $("#buttonsList").append("<div class=\"myButton\" onclick=\"restart()\">Recommencer la visite</div>");
         }
 
     }
+
+    readTextFile(file) {
+        var rawFile = new XMLHttpRequest();
+        rawFile.open("GET", file, false);
+        rawFile.onreadystatechange = function () {
+            if (rawFile.readyState === 4) {
+                if (rawFile.status === 200 || rawFile.status == 0) {
+                    var allText = rawFile.responseText;
+                    var div = document.getElementById("chat");
+                    cb = new ChatBot();
+                    cb.printText(allText, "decal-gauche");
+                }
+            }
+        }
+        rawFile.send(null);
+    }
+
+}
--- index.php
+++ index.php
@@ -1,84 +1,89 @@
 <!DOCTYPE html>
 <html style="overflow: hidden;">
 
-<head>
-    <title>HEP Campus chatbot</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
-    <link rel="icon" href="favicon.ico" />
-    <script src="js/bootstrap.min.js"></script>
-    <script src="ChatBot/CsvToArray.js"></script>
-    <script src="http://code.jquery.com/jquery.js"></script>
-    <script src="ChatBot/ChatBot.js"></script>
-    <script src="js/changementVideo.js"></script>
+    <head>
+        <title>HEP Campus chatbot</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+        <link rel="icon" href="favicon.ico" />
+        <script src="js/bootstrap.min.js"></script>
+        <script src="ChatBot/CsvToArray.js"></script>
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="ChatBot/ChatBot.js"></script>
+        <script src="js/changementVideo.js"></script>
 
-</head>
+    </head>
 
-<body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top" style="font-size:1.5em;height: 12vh">
-        <div class="container" style="margin-top: 5px;margin-bottom: 5px;">
-            <img class="animated animated fadeIn slow" src="./images/LOGO_HEP_EDUCATION_BASELINE-FR.png" alt="" style="width: 20%;">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active animated fadeInRight slow">
-                        <a class="nav-link" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <div class="dropdown">
-                            <a class="nav-link animated fadeInRight slow">Nos écoles</a>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top" style="font-size:1.5em;height: 12vh">
+            <div class="container" style="margin-top: 5px;margin-bottom: 5px;">
+                <img class="animated animated fadeIn slow" src="./images/LOGO_HEP_EDUCATION_BASELINE-FR.png" alt="" style="width: 20%;">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active animated fadeInRight slow">
+                            <a class="nav-link" href="#">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <div class="dropdown">
+                                <a class="nav-link animated fadeInRight slow">Nos écoles</a>
 
-                            <div class="dropdown-content" style="font-size: 0.8em;background-color: #343a40;">
-                                <a class="nav-link" style="border-top: solid;" href="http://www.epsi.fr/campus/campus-de-nantes/" target=_blank>EPSI</a>
-                                <a class="nav-link" style="border-top: solid;" href="http://www.ecoles-idrac.com/Idrac/Campus-de-Nantes" target=_blank>IDRAC</a>
-                                <a class="nav-link" style="border-top: solid;" href="http://www.ifag.com/campus/campus-de-nantes/" target=_blank>IFAG</a>
-                                <a class="nav-link" style="border-top: solid;" href="http://www.opensourceschool.fr/campus/nantes/" target=_blank>OPEN SOURCE SCHOOL</a>
-                                <a class="nav-link" style="border-top: solid;" href="http://www.ecoles-supdecom.com/campus/nantes/" target=_blank>SUP DE COM</a>
-                                <a class="nav-link" style="border-top: solid;" href="http://www.wis-ecoles.com/campus/campus-de-nantes/" target=_blank>WIS</a>
+                                <div class="dropdown-content" style="font-size: 0.8em;background-color: #343a40;">
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.epsi.fr/campus/campus-de-nantes/" target=_blank>EPSI</a>
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.ecoles-idrac.com/Idrac/Campus-de-Nantes" target=_blank>IDRAC</a>
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.ifag.com/campus/campus-de-nantes/" target=_blank>IFAG</a>
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.opensourceschool.fr/campus/nantes/" target=_blank>OPEN SOURCE SCHOOL</a>
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.ecoles-supdecom.com/campus/nantes/" target=_blank>SUP DE COM</a>
+                                    <a class="nav-link" style="border-top: solid;" href="http://www.wis-ecoles.com/campus/campus-de-nantes/" target=_blank>WIS</a>
+                                </div>
                             </div>
-                        </div>
-                    </li>
-                    <li class="nav-item animated fadeInRight slow">
-                        <a class="nav-link" href="./pages du site/aPropos.php">A propos</a>
-                    </li>
-                </ul>
+                        </li>
+                        <li class="nav-item animated fadeInRight slow">
+                            <a class="nav-link" href="./pages du site/aPropos.php">A propos</a>
+                        </li>
+                    </ul>
+                </div>
+        </nav>
+        <div id="changeVid">
+            <video class="animated fadeIn fast" autoplay muted loop id="myVideo" style="position: absolute;">
+                <source src="./video/Presentation_CAMPUS_HEP_Nantes.mp4" type="video/mp4">
+            </video>
+        </div>
+        <div class="pre-scrollable animated fadeInRight slow" style="position:absolute;background-color: #88888888;margin-left: 70%;max-height: 75%;border-radius: 30px;width: 30%"><br>
+
+            <div id="chat">
+                <script>
+                    cb = new ChatBot();
+                    cb.printText("question oui1", "decal-gauche");
+                </script>
             </div>
-    </nav>
-    <div id="changeVid">
-        <video class="animated fadeIn fast" autoplay muted loop id="myVideo" style="position: absolute;">
-            <source src="./video/Presentation_CAMPUS_HEP_Nantes.mp4" type="video/mp4">
-        </video>
-    </div>
-    <div class="pre-scrollable animated fadeInRight slow" style="position:absolute;background-color: #88888888;margin-left: 70%;max-height: 75%;border-radius: 30px;width: 30%"><br>
 
-        <div id="chat">
-            <script>
-                cb = new ChatBot();
-                cb.printText("question oui1", "decal-gauche");
-            </script>
+
+            <div id="buttonsList" class="choiceBox">
+                <div class="myButton" onclick="update(0)">Demarrer visite</div>
+            </div>
         </div>
+        <script>
+            function update(id) {
+                pb = new CsvToArray();
+                cb = new ChatBot(pb);
+                cb.buttonAssociator(id);
+                var div = document.getElementById("chat");
+                div.innerHTML += "<div class=decal-droit><p>" + pb.getNom(id)[0] + "</p></div>";
+                console.log(pb.getNom(id));
+                cb.readTextFile("data/" + (id + 2) + ".txt");
+            }
+            function restart() {
+                $("#buttonsList").empty();
+                $("#buttonsList").append("<div class=\"myButton\" onclick=\"update(0)\">Demarrer visite</div>");
+                $("#chat").empty();
+            }
+        </script>
 
-
-        <div id="buttonsList" class="choiceBox">
-            <div class="myButton" onclick="update(0)">Demarrer visite</div>
-        </div>
-    </div>
-    <script>
-        function update(id) {
-            pb = new CsvToArray();
-            cb = new ChatBot(pb);
-            cb.buttonAssociator(id);
-            var div = document.getElementById("chat");
-            div.innerHTML += "<div class=decal-droit><p>" + pb.getNom(id)[0] + "</p></div>";
-            console.log(pb.getNom(id));
-            cb.readTextFile("data/"+(id+2)+".txt");
-        }
-    </script>
-
-</body>
+    </body>
 
 </html>
